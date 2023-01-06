1b2314dff5136e46a17a7ca527d26c95ee104681
Güvenç Usanmaz <gusanmaz@gmail.com>
2023-01-06 04:11:09 +0300

    Revert "Grading files are added"
    This reverts commit 6f78171b968a9bf46536bdf49da2d645e76995a5.

--- docs/codes/masked/p4/B*************9/main.c
+++
@@ -1,9 +0,0 @@
-1 #include <stdio.h>
-2 #include <stdlib.h>
-3 int main(int argc, char *argv[]) {
-4 printf("location of code : %p\n", main);
-5 printf("location of heap : %p\n", malloc(100e6));
-6 int x = 3;
-7 printf("location of stack: %p\n", &x);
-8 return x;
-9 }
